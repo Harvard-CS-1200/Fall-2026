201957d2461678077a2efaea4524d7acf11cb715
salmanschowdhury <salmanchowdhury@college.harvard.edu>
2026-09-24 21:22:16 -0400

    Update ps2.py

--- psets/ps2/ps2.py
+++ psets/ps2/ps2.py
@@ -61,7 +61,7 @@ class BinarySearchTree:
         if left_size > ind and self.left is not None:
             return self.left.select(ind)
         if left_size < ind and self.right is not None:
-            return self.right.select(ind)
+            return self.right.select(ind - left_size - 1)
         return None
 
 
@@ -99,5 +99,5 @@ class BinarySearchTree:
             if self.right is None:
                 self.right = BinarySearchTree(self.debugger)
             self.right.insert(key)
-        self.calculate_sizes()
+            self.size +=1
         return self
